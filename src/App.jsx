533f564e52eabe398e327d5fcdd6b898dc201eca
Muhtoro <[200~a42440440@gmail.com>
2026-02-16 17:41:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  let nam = prompt("what is your name??")
+  let date = new Date()
+  let day = date.getDate()
+  let r = Math.random(20)
+  let b = prompt("background color is...")
+  const style = {
+    backgroundColor: b,
+    minHeight: "100vh",
+  }
+  return (
+    <div className='body' style={style}>
+      <div className="card">
+        <h1>My name is: {nam}</h1>
+        <h1>{day}</h1>
+        <h1>{r}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
