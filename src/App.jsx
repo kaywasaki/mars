4f6f7b6cc88edc73bfda4426934a8e62d615857c
Muhtoro <[200~a42440440@gmail.com>
2026-02-16 18:19:25 +0500

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   let nam = prompt("what is your name??")
   let date = new Date()
   let day = date.getDate()
-  let r = Math.random(20)
+  let r = Math.random()
   let b = prompt("background color is...")
   const style = {
     backgroundColor: b,
@@ -16,6 +16,7 @@ function App() {
         <h1>My name is: {nam}</h1>
         <h1>{day}</h1>
         <h1>{r}</h1>
+        <h1>salom</h1>
       </div>
     </div>
   );
